0af58f8edbd14e38aac753523b026190dbe0d2e6
Josue <josrangel@hotmail.com>
2021-06-26 02:50:55 -0500

    - add createNotification for button listener

--- app/src/main/java/com/jrangel/notificationexample/MainActivity.java
+++ app/src/main/java/com/jrangel/notificationexample/MainActivity.java
@@ -1,8 +1,15 @@
 package com.jrangel.notificationexample;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Tutorial
+ * https://developer.android.com/training/notify-user/build-notification
+ * https://openwebinars.net/blog/como-hacer-notificaciones-push-en-android-facil/
+ */
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createNotification(View v){
+        NotificationCreator notificationCreator = new NotificationCreator(this);
+        notificationCreator.createNotification(this, "Notification Created", "Hi, I'm a notification. I love you \uD83D\uDE18");
     }
 }
